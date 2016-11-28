54f22b5a68fd5a93b39c008c539de2deff9c5da6
Adrien Maglo <magsoft@videolan.org>
2016-11-28 19:40:21 +0100

    Add CMake as the build system

---
+++ cmake/ambisonic.pc.cmake
@@ -0,0 +1,10 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+
+Name: ambisonic-lib
+Description: Ambisonic decoding library
+Version: 0.0.1
+Libs: -L${libdir} -lambisonic
+Cflags: -I${includedir}/ambisonic
