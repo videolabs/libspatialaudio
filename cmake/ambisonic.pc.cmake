c99090629a8df783c901098236c5201ba267f0d4
Adrien Maglo <magsoft@videolan.org>
2016-11-30 14:55:54 +0100

    Fix CMake configuration file

--- cmake/ambisonic.pc.cmake
+++ cmake/ambisonic.pc.cmake
@@ -1,10 +1,10 @@
 prefix=@CMAKE_INSTALL_PREFIX@
-exec_prefix=@CMAKE_INSTALL_PREFIX@
-libdir=@LIB_INSTALL_DIR@
-includedir=@INCLUDE_INSTALL_DIR@
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
 
 Name: ambisonic-lib
 Description: Ambisonic decoding library
-Requires: 
+Version: 0.0.1
 Libs: -L${libdir} -lambisonic
 Cflags: -I${includedir}/ambisonic
