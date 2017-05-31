26a6f808bb17e20ae75ffc1c3e59680262ab98e4
Adrien Maglo <magsoft@videolan.org>
2017-05-31 14:21:13 +0200

    cmake: fix the compilation without libmysofa

--- cmake/ambisonic.pc.cmake
+++ cmake/ambisonic.pc.cmake
@@ -1,11 +1,10 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 prefix_libmysofa=@MYSOFA_ROOT_DIR@
 libdir=${prefix}/lib
-libdir_mysofa=${prefix_libmysofa}/lib
 includedir=${prefix}/include
 
 Name: ambisonic-lib
 Description: Ambisonic decoding library
 Version: 0.0.1
-Libs: -L${libdir} -L${libdir_mysofa} -lambisonic -l:libmysofa.a -lm -lz
-Cflags: -I${includedir}
+Libs: -L${libdir} -lambisonic @MYSOFA_LIB@ -lm -lz
+Cflags: -I${includedir} @MYSOFA_INCLUDE@
