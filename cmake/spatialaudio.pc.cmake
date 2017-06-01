6285a6a3e5f20e5d6508141bfc35d94854cf3cf1
Adrien Maglo <magsoft@videolan.org>
2017-06-01 10:30:30 +0200

    CMake: complete the renaming of the library to libspatialaudio

--- cmake/spatialaudio.pc.cmake
+++ cmake/spatialaudio.pc.cmake
@@ -3,8 +3,8 @@ prefix_libmysofa=@MYSOFA_ROOT_DIR@
 libdir=${prefix}/lib
 includedir=${prefix}/include
 
-Name: ambisonic-lib
-Description: Ambisonic decoding library
+Name: libspatialaudio
+Description: Spatial audio rendering library
 Version: 0.0.1
-Libs: -L${libdir} -lambisonic @MYSOFA_LIB@ -lm -lz
+Libs: -L${libdir} -lspatialaudio @MYSOFA_LIB@ -lm -lz
 Cflags: -I${includedir} @MYSOFA_INCLUDE@
