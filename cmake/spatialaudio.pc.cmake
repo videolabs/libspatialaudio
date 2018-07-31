ac2909942262ca9d712b1f444970114177ce9512
Sebastian Ramacher <sramacher@debian.org>
2018-07-31 04:33:25 +0200

    Generate version from package version info

--- cmake/spatialaudio.pc.cmake
+++ cmake/spatialaudio.pc.cmake
@@ -4,6 +4,6 @@ includedir=${prefix}/include
 
 Name: libspatialaudio
 Description: Spatial audio rendering library
-Version: 0.3.0
+Version: @PACKAGE_VERSION_MAJOR@.@PACKAGE_VERSION_MINOR@.@PACKAGE_VERSION_PATCH@
 Libs: -L${libdir} -lspatialaudio @MYSOFA_LIB@ -lm -lz
 Cflags: -I${includedir} @MYSOFA_INCLUDE@
