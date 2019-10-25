61d3f4ae39dec035cc76d714ef74710f7cb72105
Martin Saleteg <martin@saleteg.com>
2019-10-25 12:27:09 +0200

    Improve performance of speaker processing
    Pre-calculate coefficients instead of calculating for every sample

--- source/AmbisonicSpeaker.cpp
+++ source/AmbisonicSpeaker.cpp
@@ -44,24 +44,23 @@ void CAmbisonicSpeaker::Process(CBFormat* pBFSrc, unsigned nSamples, float* pfDs
     memset(pfDst, 0, nSamples * sizeof(float));
     for(niChannel = 0; niChannel < m_nChannelCount; niChannel++)
     {
+        float *in = pBFSrc->m_ppfChannels[niChannel];
+        float *out = pfDst;
+
         if(m_b3D){ /* Decode to a 3D loudspeaker array */
+            // The spherical harmonic coefficients are multiplied by (2*order + 1) to provide the correct decoder
+            // for SN3D normalised Ambisonic inputs.
+            const float coeff = m_pfCoeff[niChannel] * (2*floor(sqrt(niChannel)) + 1);
             for(niSample = 0; niSample < nSamples; niSample++)
-            {
-                // The spherical harmonic coefficients are multiplied by (2*order + 1) to provide the correct decoder
-                // for SN3D normalised Ambisonic inputs.
-                pfDst[niSample] += pBFSrc->m_ppfChannels[niChannel][niSample]
-                            * m_pfCoeff[niChannel] * (2*floor(sqrt(niChannel)) + 1);
-            }
+                *out++ += (*in++) * coeff;
         }
         else
         {    /* Decode to a 2D loudspeaker array */
+            // The spherical harmonic coefficients are multiplied by 2 to provide the correct decoder
+            // for SN3D normalised Ambisonic inputs decoded to a horizontal loudspeaker array
+            const float coeff = m_pfCoeff[niChannel] * 2.f;
             for(niSample = 0; niSample < nSamples; niSample++)
-            {
-                // The spherical harmonic coefficients are multiplied by 2 to provide the correct decoder
-                // for SN3D normalised Ambisonic inputs decoded to a horizontal loudspeaker array
-                pfDst[niSample] += pBFSrc->m_ppfChannels[niChannel][niSample]
-                            * m_pfCoeff[niChannel] * 2.f;
-            }
+                *out++ += (*in++) * coeff;
         }
     }
 }
