93f0522df17749b838966f2a95b137bac22ce86e
Peter Stitt <pstitt01@gmail.com>
2020-06-23 15:36:51 +0200

    Added option to have position interpolate over a given duration of the next frame

--- source/AmbisonicEncoder.cpp
+++ source/AmbisonicEncoder.cpp
@@ -37,15 +37,50 @@ void CAmbisonicEncoder::Refresh()
     CAmbisonicSource::Refresh();
 }
 
+void CAmbisonicEncoder::SetPosition(PolarPoint polPosition, float interpDur)
+{
+    // Set the interpolation duration
+    m_fInterpDur = interpDur;
+    // Store the last set coefficients
+    m_pfCoeffOld = m_pfCoeff;
+    // Update the coefficients
+    CAmbisonicSource::SetPosition(polPosition);
+    Refresh();
+}
+
 void CAmbisonicEncoder::Process(float* pfSrc, unsigned nSamples, CBFormat* pfDst)
 {
     unsigned niChannel = 0;
     unsigned niSample = 0;
-    for(niChannel = 0; niChannel < m_nChannelCount; niChannel++)
+    if (m_fInterpDur > 0.f)
     {
-        for(niSample = 0; niSample < nSamples; niSample++)
+        // Number of samples expected per frame
+        for (niChannel = 0; niChannel < m_nChannelCount; niChannel++)
         {
-            pfDst->m_ppfChannels[niChannel][niSample] = pfSrc[niSample] * m_pfCoeff[niChannel];
+            int nInterpSamples = (int)roundf(m_fInterpDur * nSamples);
+            float deltaCoeff = (m_pfCoeff[niChannel] - m_pfCoeffOld[niChannel]) / ((float)nInterpSamples);
+            for (niSample = 0; niSample < nInterpSamples; niSample++)
+            {
+                float fInterp = niSample * deltaCoeff;
+                pfDst->m_ppfChannels[niChannel][niSample] = pfSrc[niSample] * (fInterp*m_pfCoeff[niChannel] + (1.f-fInterp)*m_pfCoeffOld[niChannel]);
+            }
+            // once interpolation has finished
+            for (niSample = nInterpSamples; niSample < nSamples; niSample++)
+            {
+                pfDst->m_ppfChannels[niChannel][niSample] = pfSrc[niSample] * m_pfCoeff[niChannel];
+            }
+        }
+        // Set interpolation duration to zero so none is applied on next call
+        m_fInterpDur = 0.f;
+    }
+    else
+    {
+        for (niChannel = 0; niChannel < m_nChannelCount; niChannel++)
+        {
+            for (niSample = 0; niSample < nSamples; niSample++)
+            {
+                pfDst->m_ppfChannels[niChannel][niSample] = pfSrc[niSample] * m_pfCoeff[niChannel];
+            }
         }
     }
 }
