4d1c311c792247a289657027ea5500d26986c190
pstitt <pstitt01@hotmail.co.uk>
2020-06-23 15:36:51 +0200

    Added option to have position interpolate over a given duration of the next frame

--- include/AmbisonicEncoder.h
+++ include/AmbisonicEncoder.h
@@ -19,6 +19,8 @@
 #include "AmbisonicSource.h"
 #include "BFormat.h"
 
+#include <algorithm> // for std::max
+
 /// Ambisonic encoder.
 
 /** This is a basic encoder that only takes the source's azimuth an elevation
@@ -40,9 +42,21 @@ public:
     */
     void Refresh();
     /**
+        Set the position of the source with the option to interpolate over a duration
+        of the frame to the new position.
+        The duration is in the range 0.f to 1.f where 1.f interpolates over a full frame.
+    */
+    void SetPosition(PolarPoint polPosition, float interpDur = 0.f);
+    /**
         Encode mono stream to B-Format.
     */
     void Process(float* pfSrc, unsigned nSamples, CBFormat* pBFDst);
+
+private:
+    // The last set HOA coefficients
+    std::vector<float> m_pfCoeffOld;
+    // The duration [0,1] of the interpolation from the old to the new HOA coefficients
+    float m_fInterpDur = 0.f;
 };
 
 #endif // _AMBISONIC_ENCODER_H
