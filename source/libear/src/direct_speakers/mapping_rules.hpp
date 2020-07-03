723c586ad0e24c92bdd89c274f8edd9ebdd53ab4
Thomas Guillem <thomas@gllm.fr>
2020-07-03 12:07:07 +0200

    wip

--- source/libear/src/direct_speakers/mapping_rules.hpp
+++ source/libear/src/direct_speakers/mapping_rules.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <algorithm>
 #include "AdmLayouts.h"
 // This file contains code take from libear (Apache 2.0 license) and adapted to work with libspatialaudio's ADM renderer
 
