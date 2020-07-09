32e0a8fc084f913d3edebbd3ecb8bd3ca7203f4f
Thomas Guillem <thomas@gllm.fr>
2020-07-09 16:35:12 +0200

    WIP: no -lz on windows

--- cmake/spatialaudio.pc.cmake
+++ cmake/spatialaudio.pc.cmake
@@ -5,5 +5,5 @@ includedir=${prefix}/include
 Name: libspatialaudio
 Description: Spatial audio rendering library
 Version: @PACKAGE_VERSION_MAJOR@.@PACKAGE_VERSION_MINOR@.@PACKAGE_VERSION_PATCH@
-Libs: -L${libdir} -lspatialaudio @MYSOFA_LIB@ -lm -lz
+Libs: -L${libdir} -lspatialaudio @MYSOFA_LIB@ -lm
 Cflags: -I${includedir} @MYSOFA_INCLUDE@
