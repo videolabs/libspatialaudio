5cab9649dd4fd0e0127e3dcf90d8fb14f1fad8df
Thomas Guillem <thomas@gllm.fr>
2020-07-09 16:34:35 +0200

    WIP: add win64 toolchain.cmake

---
+++ toolchain.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_BUILD_TYPE RelWithDebInfo)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+set(CMAKE_AR x86_64-w64-mingw32-ar CACHE FILEPATH "Archiver")
+set(_CMAKE_TOOLCHAIN_PREFIX x86_64-w64-mingw32-)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
