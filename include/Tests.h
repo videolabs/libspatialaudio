1dc37eb11980fb8b95cdd6eb3bfd20f05ec65d58
Peter Stitt <pstitt01@hotmail.co.uk>
2024-05-08 17:24:29 +0200

    Improve AmbisonicDecoder and make it more in line with best practice.
    Compensate AmbiX inputs in decoding matrix rather than weighting the input signal.
    
    Update preset decoders.
    
    Add Octave scripts for generating header files of the presets.
    
    Add a test that can be used to verify the output of the preset decoders against data in Octave.

--- include/Tests.h
+++ include/Tests.h
@@ -596,3 +596,55 @@ void testRotation()
 	}
 
 }
+
+/** Test the loudspeaker decoder presets
+*/
+void testDecoderPresets()
+{
+	unsigned nSamples = 1;
+	unsigned order = 1;
+	bool b3D = true;
+	unsigned sampleRate = 48000;
+	auto layout = Amblib_SpeakerSetUps::kAmblib_71;
+
+	CAmbisonicEncoder ambiEnc;
+	bool success = ambiEnc.Configure(order, b3D, 0);
+	assert(success);
+
+	CAmbisonicDecoder ambiDec;
+	success = ambiDec.Configure(order, b3D, nSamples, sampleRate, layout);
+	assert(success);
+
+	unsigned nLdspk = ambiDec.GetSpeakerCount();
+
+	CBFormat inputSignal;
+	inputSignal.Configure(order, b3D, nSamples);
+	inputSignal.Reset();
+	std::vector<float> impulse(nSamples, 0.f);
+	impulse[0] = 1.f;
+	std::vector<std::vector<float>> output(inputSignal.GetChannelCount(), std::vector<float>(nSamples, 0.f));
+
+	float** ldspkOut = new float* [nLdspk];
+	for (int iLdspk = 0; iLdspk < nLdspk; ++iLdspk)
+		ldspkOut[iLdspk] = new float[nSamples];
+
+	for (float az = 0.f; az < 360.f; az += 1.f)
+	{
+		ambiEnc.SetPosition({ DegreesToRadians(az),0.f,1. }, 0.f);
+		ambiEnc.Refresh();
+		ambiEnc.Process(impulse.data(), nSamples, &inputSignal);
+
+		ambiDec.Process(&inputSignal, nSamples, ldspkOut);
+
+		for (unsigned iSamp = 0; iSamp < nSamples; ++iSamp)
+		{
+			for (unsigned iLdspk = 0; iLdspk < nLdspk; ++iLdspk)
+				std::cout << ldspkOut[iLdspk][iSamp] << ", ";
+			std::cout << std::endl;
+		}
+	}
+
+	for (unsigned iLdspk = 0; iLdspk < nLdspk; ++iLdspk)
+		delete ldspkOut[iLdspk];
+	delete[] ldspkOut;
+}
